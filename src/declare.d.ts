7d569e3ad197aca58f9cf0b137b8c09fd9ea8de3
lihui_lh <OnfEnSQSwtepmbOgttHC>
2022-02-14 17:31:56 +0800

    feat(配置tsconfig.json&vite.config.ts解决@别名问题)代码提交

--- src/declare.d.ts
+++
@@ -1,8 +0,0 @@
-//解决不能ts不能识别vue文件问题，放在src文件夹下
-declare module '*.vue' {
-	import { App, defineComponent } from 'vue'
-	const component: ReturnType<typeof defineComponent> & {
-		install(app: App): void
-	}
-	export default component
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host:'127.0.0.1',
-    port:3333,
-    open:true,
-    proxy:{
-      
-    }
-  },
-  plugins: [vue()]
+	server: {
+		host: '127.0.0.1',
+		port: 3333,
+		open: true,
+		proxy: {
+
+		}
+	},
+	resolve:{
+		alias:{
+			'@': resolve(__dirname, 'src')
+		}
+	},
+	plugins: [vue()]
 })
