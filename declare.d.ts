78dc965a772408350a39fd5341f1ad6a03a1ae42
lihui_lh <OnfEnSQSwtepmbOgttHC>
2022-02-14 17:18:26 +0800

    feat(添加ts声明文件解决ts文件中不能识别vue文件问题)代码提交

---
+++ declare.d.ts
@@ -0,0 +1,8 @@
+//解决不能ts不能识别vue文件问题，放在项目根目录文件夹下
+declare module '*.vue' {
+	import { App, defineComponent } from 'vue'
+	const component: ReturnType<typeof defineComponent> & {
+		install(app: App): void
+	}
+	export default component
+}
